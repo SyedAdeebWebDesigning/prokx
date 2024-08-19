7ca21b1b414f8408359327dd92644ff440746176
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-08-20 01:43:28 +0530

    Add new feature to handle user authentication
    This commit adds a new feature to handle user authentication. It includes changes to the login page, user model, and authentication controller.

--- app/(root)/(products)/products/page.tsx
+++ app/(root)/(products)/products/page.tsx
@@ -14,35 +14,20 @@ interface ProductsPageProps {}
 const ProductsPage = ({}: ProductsPageProps) => {
   const [products, setProducts] = useState<IProductDocument[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [limit, setLimit] = useState(8); // Start with a limit of 8
-  const [hasMore, setHasMore] = useState(true);
   const searchParams = useSearchParams();
   const category = searchParams.get("category");
 
   useEffect(() => {
     const fetchProducts = async () => {
       setIsLoading(true);
-      try {
-        const data = await getPublishableProducts(1, limit); // Adjusted to use limit
-        const productsData: IProductDocument[] = JSON.parse(
-          JSON.stringify(data),
-        );
-
-        setProducts(productsData);
-
-        // Check if there are more products to load
-        if (productsData.length < limit) {
-          setHasMore(false);
-        }
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      } finally {
-        setIsLoading(false);
-      }
+      const data = await getPublishableProducts();
+      const productsData: IProductDocument[] = JSON.parse(JSON.stringify(data));
+      setProducts(productsData);
+      setIsLoading(false);
     };
 
     fetchProducts();
-  }, [limit, category]);
+  }, []);
 
   const filteredProducts = useMemo(
     () =>
@@ -56,13 +41,7 @@ const ProductsPage = ({}: ProductsPageProps) => {
     [products, category],
   );
 
-  const loadMore = () => {
-    if (!isLoading && hasMore) {
-      setLimit((prevLimit) => prevLimit + 8); // Increment limit by 8
-    }
-  };
-
-  if (isLoading && limit === 8) {
+  if (isLoading) {
     return (
       <main className="my-20">
         <Heading>{category || "Products"} Collections</Heading>
@@ -99,14 +78,6 @@ const ProductsPage = ({}: ProductsPageProps) => {
             />
           ))}
         </section>
-        {hasMore && !isLoading && (
-          <button
-            onClick={loadMore}
-            className="mt-4 rounded bg-blue-500 px-4 py-2 text-white"
-          >
-            Load More
-          </button>
-        )}
       </MaxWidthWrapper>
     </main>
   );
